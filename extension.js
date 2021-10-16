04de1d70d7d7b307b75f40c9dbe9d139b2836697
Sahil Sonawane <sahil0sonawane@gmail.com>
2021-10-16 18:07:37 +0530

    * Add `"linksManager.config.getFavoriteTags": "flutter,dart"` in   settings.json * Or add it from settings (vscode gui) * Just replace the flutter,dart to any x number of tags

--- extension.js
+++ extension.js
@@ -2,7 +2,7 @@ const vscode = require("vscode");
 const axios = require("axios");
 
 async function activate(context) {
-  let disposable = vscode.commands.registerCommand(
+  let soQuestionsDisposable = vscode.commands.registerCommand(
     "personal-links-manager.getQuestionsFromSO",
     async function () {
       vscode.window.showInformationMessage("Search for tags");
@@ -53,7 +53,55 @@ async function activate(context) {
     }
   );
 
-  context.subscriptions.push(disposable);
+  let soFavQuestionsDisposable = vscode.commands.registerCommand(
+    "personal-links-manager.getFavQuestionsFromSO",
+    async function () {
+      var favoriteTags = vscode.workspace
+        .getConfiguration()
+        .get("linksManager.config.getFavoriteTags");
+
+      const tags = favoriteTags.split(",");
+      var soResponse;
+      await vscode.window.withProgress(
+        {
+          location: vscode.ProgressLocation.Notification,
+          title: `Fetching Questions (Tags: ${favoriteTags})`,
+        },
+        async (_) => {
+          console.log(_);
+          soResponse = await axios.get(
+            `https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&tagged=${tags
+              .map((tagStr) => tagStr.trim())
+              .join(";")}&site=stackoverflow`
+          );
+        }
+      );
+      var questions = soResponse.data.items.map((question) => {
+        return {
+          label: question.title,
+          detail: `By ${question.owner.display_name} | Answers: ${question.answer_count} | Views: ${question.view_count}`,
+          link: question.link,
+        };
+      });
+      if (questions.length === 0) {
+        vscode.window.showInformationMessage("No questions found for the tags");
+        return;
+      }
+
+      var question = await vscode.window.showQuickPick(questions, {
+        matchOnDetail: true,
+      });
+
+      if (question == null) {
+        vscode.window.showInformationMessage("No question selected");
+        return;
+      }
+      vscode.env.openExternal(question.link);
+    }
+  );
+
+  context.subscriptions.push(soQuestionsDisposable);
+  context.subscriptions.push(soFavQuestionsDisposable);
 }
 
 // this method is called when your extension is deactivated
