7c1c904b7880b3a0dd0b33275eff37cd81db835a
Sahil Sonawane <sahil0sonawane@gmail.com>
2021-10-16 16:54:43 +0530

    Added stack overflow questions search wrt tags
    * Type "Search Stack Overflow" in actions prompt
    * Type in the tags (comma-separated)
    * Wait for the results
    * Click on any question to open in browser

--- extension.js
+++ extension.js
@@ -1,36 +1,51 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
+const axios = require("axios");
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+async function activate(context) {
+  let disposable = vscode.commands.registerCommand(
+    "personal-links-manager.getQuestionsFromSO",
+    async function () {
+      vscode.window.showInformationMessage("Search for tags");
+      var inputTag = await vscode.window.showInputBox({
+        placeHolder: "Write comma-separated tags",
+      });
+      if (inputTag == "" || inputTag == null) {
+        vscode.window.showInformationMessage("No tags provided");
+        return;
+      }
+      const tags = inputTag.split(",");
+      const soResponse = await axios.get(
+        `https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&tagged=${tags
+          .map((tagStr) => tagStr.trim())
+          .join(";")}&site=stackoverflow`
+      );
+      var questions = soResponse.data.items.map((question) => {
+        return {
+          label: question.title,
+          detail: `By ${question.owner.display_name} | Answers: ${question.answer_count} | Views: ${question.view_count}`,
+          link: question.link,
+        };
+      });
 
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
+      var question = await vscode.window.showQuickPick(questions, {
+        matchOnDetail: true,
+      });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "personal-links-manager" is now active!');
+      if (question == null) {
+        vscode.window.showInformationMessage("No question selected");
+        return;
+      }
+      vscode.env.openExternal(question.link);
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('personal-links-manager.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Personal links manager!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
